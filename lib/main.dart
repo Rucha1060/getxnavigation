e9ade0d645e5c802ce29f0dec088f5d8536b19e2
Rucha1060 <ruchabakare10@gmail.com>
2022-03-29 20:19:51 +0530

    commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:getxnavigation/ui.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     
+     
+    home: MyHomePage(title:"getx navigation"),
+    
+    
+
+    );
+  }
+}
